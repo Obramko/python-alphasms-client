80d8300d60a15502f32a8d9ecfd3bab4e61a4b39
Vadym Abramchuk <abramm@gmail.com>
2014-09-07 00:27:52 +0300

    setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from distutils.core import setup
 setup(
-    name='alphasms',
-    version='0.0.1',
+    name='alphasms-client',
+    version='0.1',
+    url='https://github.com/Obramko/python-alphasms-client',
     packages=['alphasms'],
     author='Vadym Abramchuk',
     author_email='abramm@gmail.com',
     maintainer='Vadym Abramchuk',
     maintainer_email='abramm@gmail.com',
-    license='LGPL'
-    )
+    license='LGPLv2+',
+    install_requires=[
+        'requests'
+    ],
+    classifiers=[
+        'Topic :: Communications :: Telephony',
+        'License :: OSI Approved :: GNU Lesser General Public License v2 or later (LGPLv2+)',
+        'Environment :: Web Environment',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
