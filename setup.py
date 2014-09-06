f48955e728f0652664f727de465b3aab8a7ffe5c
Vadym Abramchuk <abramm@gmail.com>
2014-09-07 00:52:59 +0300

    bump

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 from distutils.core import setup
 setup(
     name='alphasms-client',
-    version='0.1',
+    version='0.1.1',
+    description='Ukrainian AlphaSMS service client API implementation for Python',
     url='https://github.com/Obramko/python-alphasms-client',
+    download_url='https://github.com/Obramko/python-alphasms-client/tarball/0.1.1',
     packages=['alphasms'],
     author='Vadym Abramchuk',
     author_email='abramm@gmail.com',
